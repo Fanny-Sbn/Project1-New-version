321ca1e21f18a3df73c7e9aa0144956500e9ba0f
Fanny Sabin <fannysabin.fs@gmail.com>
2020-06-26 09:50:44 +0200

    adding statement for game 1 and change light effect style

--- js/data-game1.js
+++ js/data-game1.js
@@ -34,6 +34,61 @@ export var facts = [
     {
         statement: "The earliest known reference to immunity goes back over one millennium",
         trueOrFalse: false,
+    },
+    {
+        statement: "Each part of the immune system has a unique function.",
+        trueOrFalse: true,
+    },
+    {
+        statement: "The human body can survive without the immune system",
+        trueOrFalse: false,
+    },
+    {
+        statement: "The most important part of the Immune system is the blood cells",
+        trueOrFalse: true,
+    },
+    {
+        statement: "Insomnia detoriates the immune system",
+        trueOrFalse: true,
+    },
+    {
+        statement: "Human gut has 80% of the immune system",
+        trueOrFalse: true,
+    },
+    {
+        statement: "The first response to an infection is called inflammation",
+        trueOrFalse: true,
+    },
+    {
+        statement: "A fever indicates a dysfunctional immune system",
+        trueOrFalse: false,
+    },
+    {
+        statement: "Low lymphocyte count means infection",
+        trueOrFalse: false,
+    }, 
+    {
+        statement: "Exposure to the sun is a great way to improve the functionality of your immune system",
+        trueOrFalse: true,
+    },
+    {
+        statement: "The body's defenses against infection die and regenerate every day.",
+        trueOrFalse: true,
+    },
+    {
+        statement: "How friendly you're feeling could be linked to your immune system",
+        trueOrFalse: true,
+    },
+    {
+        statement: "The immune system can be tricked",
+        trueOrFalse: true,
+    },
+    {
+        statement: "Infection will usually lead to an immune response",
+        trueOrFalse: true,
+    },
+    {
+        statement: "Only invertebrates have specific immune responses",
+        trueOrFalse: false,
     }
-
 ]
