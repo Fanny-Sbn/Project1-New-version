b0d180f9a38004098d0309dc85a177434ff97941
Fanny Sabin <fannysabin.fs@gmail.com>
2020-06-26 13:32:32 +0200

    previous version

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ const states = [
 var index = 0;
 
 function displayView(html) {
-  if (index > 0) document.getElementById("dynamic-css").setAttribute("href", states[index].css);
+  document.getElementById("dynamic-css").setAttribute("href", states[index].css);
   document.getElementById("board").innerHTML = html;
   states[index].js(loadPage); // loadPage va être rappelé
   index++;
